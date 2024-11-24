88d3bf16228fdd14edfaf3075c90b3730c35750b
dev-vilela <paulov4@hotmail.com>
2024-11-24 18:42:56 -0300

    Aula 09 sobre as propriedades Undefined e Null

---
+++ Aula09[UNDEFINED E NULL]/index.js
@@ -0,0 +1,12 @@
+/*----------------Undefined e Null-------------------- */
+
+var nome =null;
+var sobreNome = "Vilela";
+
+console.log(nome);
+console.log(sobreNome);
+
+
+nome = "Paulo";
+
+console.log(nome);
