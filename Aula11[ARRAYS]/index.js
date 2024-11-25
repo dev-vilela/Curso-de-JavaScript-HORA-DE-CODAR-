b7d9ea88e30506ccabb20896f87a1928c7d8b4c3
dev-vilela <paulov4@hotmail.com>
2024-11-25 20:12:45 -0300

    Aula 11 sobre a propriedade Array

---
+++ Aula11[ARRAYS]/index.js
@@ -0,0 +1,14 @@
+/*---------------ARRAYS------------ */
+
+var arr = [5, "Paulo", true,{teste: 1, test: 2}];
+
+console.log(arr)
+
+var arr2 = [2,3,4,5,6]
+
+console.log(arr[1])
+console.log(arr2[0])
+
+ arr[4] = 10;//INSERINDO VALOR NO ARRAY
+
+console.log(arr)
