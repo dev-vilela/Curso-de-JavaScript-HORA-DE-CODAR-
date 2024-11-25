611d45dcba932bfadd212976365f84d703393fb2
dev-vilela <paulov4@hotmail.com>
2024-11-25 20:02:01 -0300

    Aula sobre tipo de dados: Objeto

---
+++ Aula10[OBJETO]/index.js
@@ -0,0 +1,18 @@
+/*---------------OBJETO------------ */
+
+var obj = {
+    nome: "Paulo",
+    idade: 22,
+    profissao: "Desenvolvedor"
+}
+
+console.log(obj);
+
+console.log(typeof obj)
+
+console.log("Meu nome é " + obj.nome + " tenho " + obj.idade + " anos")
+
+obj.graduacao = "Analise e Desenvolvimento de Sistema"
+
+console.log("Meu nome é " + obj.nome + " tenho " + obj.idade + " anos" + " sou formado no curso de  " + obj.graduacao)
+
