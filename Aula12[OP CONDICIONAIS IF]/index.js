18950fbe5413e4f1be03c4a958f0f27761a647c9
dev-vilela <paulov4@hotmail.com>
2024-11-27 06:49:22 -0300

    Aula 12 sobre a propriedade IF

---
+++ Aula12[OP CONDICIONAIS IF]/index.js
@@ -0,0 +1,16 @@
+/*----------------OPERADORES CONDICIONAIS---------------
+
+1- São instruções que podem determinar o fluxo de uma aplicação;
+2- Amplamentes utilizados, deixa os sofwates dinâmicos;
+3- Exemplos são: if, else if, else;
+
+*/
+
+var idade = 16;
+
+
+if(idade >= 18){
+    console.log("Pode tirar a CNH." + idade + " anos.")
+}else{
+    console.log("Não pode tirar a CNH menor de idade com " + idade + " anos")
+}
