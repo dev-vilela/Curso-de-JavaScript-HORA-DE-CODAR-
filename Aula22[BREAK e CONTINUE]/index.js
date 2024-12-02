3813b8a2654eb8b78cdf7ac71d4a4acfd05575a2
dev-vilela <paulov4@hotmail.com>
2024-12-02 19:12:03 -0300

    Aula sobre a propriedade BREAK e CONTINUE

---
+++ Aula22[BREAK e CONTINUE]/index.js
@@ -0,0 +1,12 @@
+/*-----------------BREAK E CONTINUE--------------------- */
+
+for(var i = 10; i > 0; i-- ){
+
+    console.log(i)
+
+    if(i === 5){
+        break;
+    }
+}
+
+console.log("BREAK")
